48d93723dddf4c8e9448b1414d59e53b416bf5d6
Will Fisher <will@teknofire.net>
2015-01-15 09:41:08 -0900

    fixing issue with missing class for georuby

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'georuby'
+require 'geo_ruby/ewk'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
